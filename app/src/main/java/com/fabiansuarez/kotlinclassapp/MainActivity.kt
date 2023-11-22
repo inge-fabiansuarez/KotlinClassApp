44faae44c054ed8eba4de3797e8bef09e2c5b146
Fabian Suarez <inge.fabiansuarez@gmail.com>
2023-11-21 22:44:43 -0500

    Se agrego databinding y se asocio con el activity

--- app/src/main/java/com/fabiansuarez/kotlinclassapp/MainActivity.kt
+++ app/src/main/java/com/fabiansuarez/kotlinclassapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.fabiansuarez.kotlinclassapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.fabiansuarez.kotlinclassapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tvTitle.text = "ESTE ES MI TITULO PERSONALIZADO"
     }
 }
