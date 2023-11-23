222a1eeb9928f70dd6a319463c66d7838441c5a2
Fabian Suarez <inge.fabiansuarez@gmail.com>
2023-11-22 19:57:32 -0500

    Se creo la parte de diseño del recycle view

---
+++ app/src/main/java/com/fabiansuarez/kotlinclassapp/ProductListActivityViewModel.kt
@@ -0,0 +1,18 @@
+package com.fabiansuarez.kotlinclassapp
+
+import androidx.lifecycle.ViewModel
+
+class ProductListActivityViewModel : ViewModel() {
+
+    val products:ArrayList<Product> = arrayListOf()
+
+    fun loadProducts(){
+        products.apply {
+            clear()
+            add(Product(name="Computador", price = 50000.0))
+            add(Product(name="Teclado", price = 20000.0))
+            add(Product(name="Disco Duro", price = 30000.0))
+            add(Product(name="Monitor", price = 50000.0))
+        }
+    }
+}
